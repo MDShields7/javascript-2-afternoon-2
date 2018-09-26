ea04bc7e6ec2ce0301378f76378984aec4ca7429
Matt Shields <matthew.duncan.shields@gmail.com>
2018-09-25 22:07:10 -0700

    complete required, pass tests

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first (arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last (arr){
+  return arr[arr.length-1];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper (fam){
+  for (let i = 0; i<arr.length; i++){
+    alert(fam[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper (charArr){
+  for(let i = arr.length-1; i >= 0; i--){
+    alert(charArr[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +90,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(numArr){
+  let newArr = [];
+  for (let i = 0; i < numArr.length; i++){
+    if (numArr[i] % 2 === 0){
+      newArr.push(numArr[i])
+    }
+  }
+  return newArr;
+}
 
 
 
@@ -106,7 +126,16 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
-
+function divider(arr){
+  let evensArr = [];
+  let oddsArr = [];
+  let newArr = [evensArr, oddsArr];
+  for (let i = 0; i < arr.length; i++){
+    let item = arr[i];  
+    item % 2 === 0 ? evensArr.push(item): oddsArr.push(item);
+  }
+  return newArr;
+}
 
 
 ////////// PROBLEM 7 //////////
